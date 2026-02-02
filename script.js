c0e7d6915980d721c187a33c84195e1b347d24b1
SoyJemoox <fabiianm745@gmail.com>
2026-02-02 09:41:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
 
+btn.addEventListener("click", () => {
+  msg.textContent = "🚀 JavaScript funcionando correctamente";
+});
